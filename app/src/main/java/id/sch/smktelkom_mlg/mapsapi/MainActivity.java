1ced3fb1b04d24994eb0afb18f5a5b611d2dc7b7
zafiracandra <zafiracandrawulan@gmail.com>
2017-11-23 18:12:29 +0700

    Quiz : Circles

--- app/src/main/java/id/sch/smktelkom_mlg/mapsapi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/mapsapi/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.mapsapi;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,25 +10,22 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     static final CameraPosition INDONESIA = CameraPosition.builder()
-            .target(new LatLng(-6.175392, 106.827178))
-            .zoom(5)
+            .target(new LatLng(-6.967854, 107.583750))
+            .zoom(15)
             .bearing(0)
             .tilt(45)
             .build();
     GoogleMap m_map;
     boolean mapReady = false;
-    LatLng IND = new LatLng(-6.175392, 106.827178);
-    LatLng FRC = new LatLng(48.858270, 2.294509);
-    LatLng USA = new LatLng(38.897678, -77.036477);
-    LatLng AUS = new LatLng(-33.856820, 151.215279);
-    MarkerOptions Indonesia, France, UnitedState, Australia;
+    LatLng IND = new LatLng(-6.967854, 107.583750);
+    MarkerOptions Indonesia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +33,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         Indonesia = new MarkerOptions()
-                .position(new LatLng(-6.175392, 106.827178))
-                .title("Monumen Nasional")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
-
-        France = new MarkerOptions()
-                .position(new LatLng(48.858270, 2.294509))
-                .title("Eiffel Tower")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
-
-        UnitedState = new MarkerOptions()
-                .position(new LatLng(38.897678, -77.036477))
-                .title("The White House")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
-
-        Australia = new MarkerOptions()
-                .position(new LatLng(-33.856820, 151.215279))
-                .title("Sydney Opera House")
+                .position(new LatLng(-6.967854, 107.583750))
+                .title("Perumahan")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
@@ -67,15 +50,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapReady = true;
         m_map = map;
         map.moveCamera(CameraUpdateFactory.newCameraPosition(INDONESIA));
-        map.addPolyline(new PolylineOptions().geodesic(true)
-                .add(IND)
-                .add(AUS)
-                .add(FRC)
-                .add(USA));
+        map.addCircle(new CircleOptions()
+                .center(IND)
+                .radius(500)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(64, 0, 255, 0)));
         m_map.addMarker(Indonesia);
-        m_map.addMarker(France);
-        m_map.addMarker(UnitedState);
-        m_map.addMarker(Australia);
     }
-
 }
